759b510923987c030eecc1834254f5d043d2a121
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-09-16 12:07:57 +0700

    updated: database

--- database/booking.sql
+++ database/booking.sql
@@ -22,20 +22,20 @@ USE `booking`;
 -- Dumping structure for table booking.bookings
 CREATE TABLE IF NOT EXISTS `bookings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `customerid` int(11) NOT NULL,
-  `scheduleid` int(11) NOT NULL,
+  `customer` int(11) NOT NULL,
+  `schedule` int(11) NOT NULL,
   `seatnum` int(11) NOT NULL,
   `time` timestamp NOT NULL DEFAULT current_timestamp(),
   `status` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_bookings_customers` (`customerid`),
-  KEY `FK_bookings_schedules` (`scheduleid`),
-  CONSTRAINT `FK_bookings_customers` FOREIGN KEY (`customerid`) REFERENCES `customers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_bookings_schedules` FOREIGN KEY (`scheduleid`) REFERENCES `schedules` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `FK_bookings_customers` (`customer`) USING BTREE,
+  KEY `FK_bookings_schedules` (`schedule`) USING BTREE,
+  CONSTRAINT `FK_bookings_customers` FOREIGN KEY (`customer`) REFERENCES `customers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_bookings_schedules` FOREIGN KEY (`schedule`) REFERENCES `schedules` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table booking.bookings: ~1 rows (approximately)
-INSERT INTO `bookings` (`id`, `customerid`, `scheduleid`, `seatnum`, `time`, `status`) VALUES
+-- Dumping data for table booking.bookings: ~0 rows (approximately)
+INSERT INTO `bookings` (`id`, `customer`, `schedule`, `seatnum`, `time`, `status`) VALUES
 	(1, 1, 1, 12, '2024-09-15 14:06:03', 1);
 
 -- Dumping structure for table booking.buses
@@ -44,14 +44,14 @@ CREATE TABLE IF NOT EXISTS `buses` (
   `busnumber` varchar(10) NOT NULL DEFAULT '',
   `seatcount` int(11) NOT NULL,
   `bustype` varchar(50) NOT NULL DEFAULT '',
-  `driverid` int(11) NOT NULL,
+  `driver` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_buses_drivers` (`driverid`),
-  CONSTRAINT `FK_buses_drivers` FOREIGN KEY (`driverid`) REFERENCES `drivers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `FK_buses_drivers` (`driver`) USING BTREE,
+  CONSTRAINT `FK_buses_drivers` FOREIGN KEY (`driver`) REFERENCES `drivers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table booking.buses: ~1 rows (approximately)
-INSERT INTO `buses` (`id`, `busnumber`, `seatcount`, `bustype`, `driverid`) VALUES
+INSERT INTO `buses` (`id`, `busnumber`, `seatcount`, `bustype`, `driver`) VALUES
 	(2, '78E177777', 26, 'Vip', 1);
 
 -- Dumping structure for table booking.customers
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `customers` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table booking.customers: ~1 rows (approximately)
+-- Dumping data for table booking.customers: ~0 rows (approximately)
 INSERT INTO `customers` (`id`, `name`, `phone`, `email`, `address`) VALUES
 	(1, 'Pham Van Du', '115', 'abc@gmail.com', 'New Youth');
 
@@ -77,42 +77,42 @@ CREATE TABLE IF NOT EXISTS `drivers` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table booking.drivers: ~1 rows (approximately)
+-- Dumping data for table booking.drivers: ~0 rows (approximately)
 INSERT INTO `drivers` (`id`, `name`, `license`, `phone`) VALUES
 	(1, 'Duong Thanh Truong', '3121410546', '0386094783');
 
 -- Dumping structure for table booking.ewalletpay
 CREATE TABLE IF NOT EXISTS `ewalletpay` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `paymentid` int(11) NOT NULL,
+  `payment` int(11) NOT NULL,
   `provider` varchar(50) NOT NULL DEFAULT '0',
   `transactionid` int(11) NOT NULL,
   `status` int(11) NOT NULL,
   `time` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `FK_ewalletpay_payments` (`paymentid`),
   KEY `FK_ewalletpay_ewalletprovider` (`provider`),
-  CONSTRAINT `FK_ewalletpay_payments` FOREIGN KEY (`paymentid`) REFERENCES `payments` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `FK_ewalletpay_payments` (`payment`) USING BTREE,
+  CONSTRAINT `FK_ewalletpay_payments` FOREIGN KEY (`payment`) REFERENCES `payments` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table booking.ewalletpay: ~1 rows (approximately)
-INSERT INTO `ewalletpay` (`id`, `paymentid`, `provider`, `transactionid`, `status`, `time`) VALUES
+-- Dumping data for table booking.ewalletpay: ~0 rows (approximately)
+INSERT INTO `ewalletpay` (`id`, `payment`, `provider`, `transactionid`, `status`, `time`) VALUES
 	(1, 1, 'Momo', 1111, 0, '2024-09-15 14:11:18');
 
 -- Dumping structure for table booking.payments
 CREATE TABLE IF NOT EXISTS `payments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `bookingid` int(11) NOT NULL,
+  `booking` int(11) NOT NULL,
   `amount` double NOT NULL DEFAULT 0,
   `method` varchar(50) NOT NULL DEFAULT '',
   `time` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `FK_payments_bookings` (`bookingid`),
-  CONSTRAINT `FK_payments_bookings` FOREIGN KEY (`bookingid`) REFERENCES `bookings` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `FK_payments_bookings` (`booking`) USING BTREE,
+  CONSTRAINT `FK_payments_bookings` FOREIGN KEY (`booking`) REFERENCES `bookings` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table booking.payments: ~1 rows (approximately)
-INSERT INTO `payments` (`id`, `bookingid`, `amount`, `method`, `time`) VALUES
+-- Dumping data for table booking.payments: ~0 rows (approximately)
+INSERT INTO `payments` (`id`, `booking`, `amount`, `method`, `time`) VALUES
 	(1, 1, 320000, 'Ví điện tử', '2024-09-15 14:08:32');
 
 -- Dumping structure for table booking.routes
@@ -125,27 +125,27 @@ CREATE TABLE IF NOT EXISTS `routes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table booking.routes: ~1 rows (approximately)
+-- Dumping data for table booking.routes: ~0 rows (approximately)
 INSERT INTO `routes` (`id`, `from`, `to`, `distance`, `duration`) VALUES
 	(1, 'TP Hồ Chí Minh', 'Phú Yên', 540, '10:00:00');
 
 -- Dumping structure for table booking.schedules
 CREATE TABLE IF NOT EXISTS `schedules` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `busid` int(11) NOT NULL,
-  `routeid` int(11) NOT NULL,
+  `bus` int(11) NOT NULL,
+  `route` int(11) NOT NULL,
   `departure` timestamp NOT NULL DEFAULT current_timestamp(),
   `arrival` timestamp NOT NULL DEFAULT current_timestamp(),
   `price` double NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
-  KEY `FK_schedules_buses` (`busid`),
-  KEY `FK_schedules_routes` (`routeid`),
-  CONSTRAINT `FK_schedules_buses` FOREIGN KEY (`busid`) REFERENCES `buses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_schedules_routes` FOREIGN KEY (`routeid`) REFERENCES `routes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `FK_schedules_buses` (`bus`) USING BTREE,
+  KEY `FK_schedules_routes` (`route`) USING BTREE,
+  CONSTRAINT `FK_schedules_buses` FOREIGN KEY (`bus`) REFERENCES `buses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_schedules_routes` FOREIGN KEY (`route`) REFERENCES `routes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table booking.schedules: ~1 rows (approximately)
-INSERT INTO `schedules` (`id`, `busid`, `routeid`, `departure`, `arrival`, `price`) VALUES
+-- Dumping data for table booking.schedules: ~0 rows (approximately)
+INSERT INTO `schedules` (`id`, `bus`, `route`, `departure`, `arrival`, `price`) VALUES
 	(1, 2, 1, '2024-09-15 12:00:00', '2024-09-14 22:00:00', 320000);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
