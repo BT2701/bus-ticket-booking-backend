e2ad220e434dc68d6df20f1675cda91f04a7d0a6
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-10-09 15:03:28 +0700

    completed: schedule detail

--- src/main/java/com/example/demo/Controller/CategoryCTL.java
+++ src/main/java/com/example/demo/Controller/CategoryCTL.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "${frontend.url}", allowCredentials = "true")
 public class CategoryCTL {
     @Autowired
     private CategorySV categorySV;
