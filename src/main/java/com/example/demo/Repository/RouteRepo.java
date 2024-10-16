49c17de73700d6825bb3e05c3101a754533c260f
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-10-16 11:56:08 +0700

    add scheduleid to route query

--- src/main/java/com/example/demo/Repository/RouteRepo.java
+++ src/main/java/com/example/demo/Repository/RouteRepo.java
@@ -23,7 +23,7 @@ public interface RouteRepo extends JpaRepository<Route, Integer> {
 
     @Query("SELECT c.name as busType, s.departure, s.arrival, s.price, "
             + "(c.seat_count - COALESCE(SUM(CASE WHEN bk.status = 1 THEN 1 ELSE 0 END), 0)) as remainingSeats, "
-            + "r.duration, f.name as fromStation, t.name as toStation "
+            + "r.duration, f.name as fromStation, t.name as toStation, s.id  "
             + "FROM schedules s "
             + "JOIN s.bus b "
             + "JOIN b.category c "
